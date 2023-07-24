37e6b62acac95e431fb849eb7281d95a3ce98600
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2023-07-24 17:36:39 +0300

    feat: display stories

---
+++ script.js
@@ -0,0 +1,55 @@
+ const timestamp = () => {
+    let timeIndex = 1678166046264 / 1000;
+    let random = Math.floor(Math.random() * 1000);
+  
+    return Math.round(timeIndex - random);
+  };
+  
+ const getCurrentSkin = () => {
+    return  {
+        avatars: true,
+        list: false,
+        autoFullScreen: false,
+        cubeEffect: true,
+        paginationArrows: false
+    };
+  };
+
+const getStories = async () => {
+
+    try {
+        const res = await fetch('./data.json')
+        const json = await res.json()
+        const slides = json.items.map(item => (
+            {
+                id: item.id, 
+                photo: item.coverImageUrl,
+                name: '',
+                time: timestamp(),
+                items: item.slides.map(slide => (
+                    {
+                        id: slide.id,
+                        type: 'photo',
+                        length:3,
+                        src: slide.imageUrl,
+                        preview: slide.imageUrl,
+                        link: '',
+                        linkText: false,
+                        time: timestamp()
+                    }
+                ))
+            }
+        ))
+
+        return slides
+    }
+    catch (error)
+    {
+        console.error(error);
+        return []
+    }
+}
+
+export default {getStories, getCurrentSkin, timestamp}
+  
+
