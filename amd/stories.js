c195e2ea66b1af8f28c3ae87331fb8213d11b12d
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2023-07-25 12:05:50 +0300

    fix: import & export

--- amd/stories.js
+++ amd/stories.js
@@ -1,21 +1,22 @@
 import help from '../script.js'
 
-const Zuck = require('zuck.js');
+import Zuck from 'zuck.js'
 
-module.exports = {
-    drawStories: (data, id) => {
-        let stories = help.getStories(data);
-        
-        Zuck(document.querySelector(id), {
-            backNative: true,
-            previousTap: true,
-            skin: 'Default',
-            autoFullScreen: false,
-            avatars: true,
-            paginationArrows: false,
-            list: false,
-            cubeEffect: true,
-            localStorage: true,
-            stories
-        });}
+const drawStories = (data, id) => {
+    let stories = help.getStories(data);
+
+    Zuck(document.querySelector(id), {
+        backNative: true,
+        previousTap: true,
+        skin: 'Default',
+        autoFullScreen: false,
+        avatars: true,
+        paginationArrows: false,
+        list: false,
+        cubeEffect: true,
+        localStorage: true,
+        stories
+    });
 }
+
+export default drawStories
