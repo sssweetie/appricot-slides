4169381203d7d4ae340b251746ced63d9ac67b3d
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2023-07-25 11:53:29 +0300

    feat: export modules

--- amd/stories.js
+++ amd/stories.js
@@ -1,21 +1,21 @@
 import help from '../script.js'
 
-import Zuck from 'zuck.js';
+const Zuck = require('zuck.js');
 
-const drawStories = (data, id) => {
-let stories = help.getStories(data);
-
-Zuck(document.querySelector(id), {
-    backNative: true,
-    previousTap: true,
-    skin: 'Default',
-    autoFullScreen: false,
-    avatars: true,
-    paginationArrows: false,
-    list: false,
-    cubeEffect: true,
-    localStorage: true,
-    stories
-});}
-
-export default drawStories
+module.exports = {
+    drawStories: (data, id) => {
+        let stories = help.getStories(data);
+        
+        Zuck(document.querySelector(id), {
+            backNative: true,
+            previousTap: true,
+            skin: 'Default',
+            autoFullScreen: false,
+            avatars: true,
+            paginationArrows: false,
+            list: false,
+            cubeEffect: true,
+            localStorage: true,
+            stories
+        });}
+}
