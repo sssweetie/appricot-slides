00c1c2dcdfccf035be2e15e203188cd6944b703d
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2023-07-25 16:35:17 +0300

    feat: simple one

--- common/stories.js
+++ common/stories.js
@@ -1,20 +1,20 @@
-// import getStories from '../script.js'
+import getStories from '../script.js'
 
-// const drawStories = (data, id) => {
-//     let stories = getStories(data);
+const drawStories = (data, id) => {
+    let stories = getStories(data);
 
-//     window.Zuck(document.querySelector(id), {
-//         backNative: true,
-//         previousTap: true,
-//         skin: 'Default',
-//         autoFullScreen: false,
-//         avatars: true,
-//         paginationArrows: false,
-//         list: false,
-//         cubeEffect: true,
-//         localStorage: true,
-//         stories
-//     });
-// }
+    window.Zuck(document.querySelector(id), {
+        backNative: true,
+        previousTap: true,
+        skin: 'Default',
+        autoFullScreen: false,
+        avatars: true,
+        paginationArrows: false,
+        list: false,
+        cubeEffect: true,
+        localStorage: true,
+        stories
+    });
+}
 
-// export default drawStories
+export default drawStories
